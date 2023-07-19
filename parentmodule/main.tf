2ea5539107747cf0167d508e02821a0a4e2d0e46
Swaranjalsingh <swaranjal3905@gmail.com>
2023-07-19 22:09:07 +0530

    scsc

--- parentmodule/main.tf
+++ parentmodule/main.tf
@@ -9,6 +9,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+  client_id       = "123a1124-f398-47e9-95fa-e2aa97a0a9c7"
+  client_secret   = "g2R8Q~E7zYMOA6jBXW6lAmp_UVUiiVARM5DcIcqu"
+  tenant_id       = "fb9886a0-c37e-4427-82a5-a40305272af6"
+  subscription_id = "bfa711a5-2a46-4ae9-b13f-cfc3648d8f94"
 }
 
 
@@ -23,12 +27,6 @@ resource "azurerm_storage_account" "swara" {
   location                 = azurerm_resource_group.swara.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
-
-  tags = {
-    ApplicationName = var.name
-    Owner           = ""
-    Client          = ""
-  }
 }
 
 
